e92b5fcfc1856fc967c718282497cece5a6d9bef
greenknightca <73036877+greenknightca@users.noreply.github.com>
2020-10-19 11:23:28 +0200

    Update gk-wc-backordered-status.php
    Now tracks variations stock quantities, as well as non stock managed products simply set to 'On backorder'.

--- gk-wc-backordered-status.php
+++ gk-wc-backordered-status.php
@@ -45,20 +45,25 @@ add_filter( 'wc_order_statuses', 'gk_add_backordered_to_order_statuses' );
 
 function gk_if_backordered( $order_id, $old_status, $new_status) {
     if( $old_status == 'backordered' && $new_status == 'processing' ){
-      return;
+      return; //We don't want to get stuck in a loop of constant backorders.
     }
     $order = new WC_Order( $order_id );
     if( $order->get_status()!='processing' ){
-      return;
+      return; //We only change orders to the backordered status is the payment is successful.
     }
     $items = $order->get_items();
     foreach ( $items as $item ) {
-      $product = wc_get_product($item['product_id']);
-
-        if ( $product->get_backorders()!='no' ) {
-          $order->update_status('backordered');
+      $product = wc_get_product(($item['variation_id']!=0?$item['variation_id']:$item['product_id'])); //Check if the product is a variation or not.
+      if( $product->get_manage_stock() ){ //Check if the product manages stock.
+        if ( $product->get_backorders()!='no' && $product->get_stock_quantity() < 0) {
+          $order->update_status('backordered'); //If the product allows for backorders, and the available stock is less than 0, set order to backordered.
           return;
         }
+      }
+      else if( $product->get_stock_status() == 'onbackorder' ){ //Check if the non stock managed product has a stock status of 'onbackorder'.
+          $order->update_status('backordered');
+          return;
+      }
     }
 }
 
